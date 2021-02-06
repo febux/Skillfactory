659a842166a178ec0d100d4f3f2e6aa41d2fc9a9
ALEXANDER <deadcove@mail.ru>
2021-02-06 22:12:48 +0300

    update of commands v2

--- ProjectD2_4/NewsPaper/news/models.py
+++ ProjectD2_4/NewsPaper/news/models.py
@@ -3,26 +3,26 @@ from django.contrib.auth.models import User
 
 
 class Author(models.Model):
-    objects = models.Manager()
+    # objects = models.Manager()
     rating_author = models.IntegerField(default=0)
     author = models.OneToOneField(User, on_delete=models.CASCADE)
 
     def update_rating(self):
-        posts = Post.objects.filter(author=self.id)  # все посты автора
+        posts = Post.objects.filter(author_post=self.id)  # все посты автора
         rating_post = sum([a.rating_post * 3 for a in posts])  # рейтинг каждого поста автора умножен на 3
-        rating_comment = sum([a.rating_comment for a in Comment.objects.filter(author=self.author)])
+        rating_comment = sum([a.rating_comment for a in Comment.objects.filter(author_comment=self.author)])
         # сумма лайков/дислайков к комментам автора
-        all_to_post_comment_rating = sum([a.rating_comment for a in Comment.objects.filter(post__in=posts)])
+        all_to_post_comment_rating = sum([a.rating_comment for a in Comment.objects.filter(post_comment__in=posts)])
         # сумма лайков/дислайков всех комментов к постам автора
         self.rating_author = rating_post + rating_comment + all_to_post_comment_rating
         self.save()
 
     def __str__(self):
-        return self.author
+        return self.author.username
 
 
 class Category(models.Model):
-    objects = models.Manager()
+    # objects = models.Manager()
     category_name = models.TextField(unique=True)
 
     def __str__(self):
@@ -30,13 +30,13 @@ class Category(models.Model):
 
 
 class Post(models.Model):
-    objects = models.Manager()
+    # objects = models.Manager()
     article = 'AR'
     news = 'NW'
     unknown = 'UK'
     POST_TYPE = [(article, 'Статья'), (news, 'Новость'), (unknown, 'Unknown')]
 
-    post = models.ForeignKey(Author, on_delete=models.CASCADE)
+    author_post = models.ForeignKey(Author, on_delete=models.CASCADE)
     type_post = models.CharField(max_length=2, choices=POST_TYPE, default=unknown)
     category_post = models.ManyToManyField(Category, through='PostCategory')
     date_post = models.DateTimeField(auto_now_add=True)
@@ -66,7 +66,7 @@ class PostCategory(models.Model):
 
 
 class Comment(models.Model):
-    objects = models.Manager()
+    # objects = models.Manager()
     post_comment = models.ForeignKey(Post, on_delete=models.CASCADE)
     author_comment = models.ForeignKey(User, on_delete=models.CASCADE)
     text_comment = models.TextField()
--- ProjectD2_4/commands.py
+++ ProjectD2_4/commands.py
@@ -1,5 +1,5 @@
 from django.contrib.auth.models import User
-from NewsPaper.news.models import Author, Post, Comment, Category
+from news.models import Author, Post, Comment, Category
 
 #  Создаем пользователей
 User.objects.create(username='Abdullah', password='qwerty')
@@ -10,45 +10,45 @@ Author.objects.create(author=User.objects.get(id=1))
 Author.objects.create(author=User.objects.get(id=2))
 
 # Создаем категории
-Category.objects.create(name='IT')
-Category.objects.create(name='AI')
-Category.objects.create(name='Business')
-Category.objects.create(name='New Tech')
+Category.objects.create(category_name='IT')
+Category.objects.create(category_name='AI')
+Category.objects.create(category_name='Business')
+Category.objects.create(category_name='New Tech')
 
 # Создаем новости и статьи
-Post.objects.create(author=Author.objects.get(id=1), type='AR',
-                    header='Article 1',
-                    article_text='Text of Article 1')
+Post.objects.create(author_post=Author.objects.get(id=1), type_post='AR',
+                    header_post='Article 1',
+                    text_post='Text of Article 1')
 
-Post.objects.create(author=Author.objects.get(id=1), type='AR',
-                    header='Article 2',
-                    article_text='Text of Article 2')
+Post.objects.create(author_post=Author.objects.get(id=1), type_post='AR',
+                    header_post='Article 2',
+                    text_post='Text of Article 2')
 
-Post.objects.create(author=Author.objects.get(id=1), type='NW',
-                    header='News',
-                    article_text='Text of News')
+Post.objects.create(author_post=Author.objects.get(id=1), type_post='NW',
+                    header_post='News',
+                    text_post='Text of News')
 
 # Присваиваем категории новости
 post = Post.objects.get(id=1)
-post.category.add(Category.objects.get(id=1))
-post.category.add(Category.objects.get(id=2))
+post.category_post.add(Category.objects.get(id=1))
+post.category_post.add(Category.objects.get(id=2))
 
 post = Post.objects.get(id=2)
-post.category.add(Category.objects.get(id=2))
-post.category.add(Category.objects.get(id=4))
+post.category_post.add(Category.objects.get(id=2))
+post.category_post.add(Category.objects.get(id=4))
 
 # Создаем комментарии
 author = User.objects.get(id=2)
 post = Post.objects.get(id=1)
-Comment.objects.create(post=post, author=author, text='Comment text')
+Comment.objects.create(post_comment=post, author_comment=author, text_comment='Comment text')
 author = User.objects.get(id=1)
-Comment.objects.create(post=post, author=author, text='Comment text 1')
+Comment.objects.create(post_comment=post, author_comment=author, text_comment='Comment text 1')
 
 author = User.objects.get(id=1)
 post = Post.objects.get(id=2)
-Comment.objects.create(post=post, author=author, text='Comment text 2')
+Comment.objects.create(post_comment=post, author_comment=author, text_comment='Comment text 2')
 author = User.objects.get(id=2)
-Comment.objects.create(post=post, author=author, text='Comment text 3')
+Comment.objects.create(post_comment=post, author_comment=author, text_comment='Comment text 3')
 
 # Ставим лайки комментариям
 comments = Comment.objects.all()
@@ -57,30 +57,30 @@ comments[2].like()
 comments[1].dislike()
 comments[3].like()
 comments[3].dislike()
-comments[4].like()
+comments[0].like()
 
 # Ставим лайк для поста
 post.like()
 
 # Меняем рейтинг посту
-post.post_rating = 5
+post.rating_post = 5
 post.save()
 
 # Обновляем рейтинги авторов
 for auth in Author.objects.all():
-    auth.update_raiting()
+    auth.update_rating()
 
 # Выводим имя и рейтинг лучшего пользоветеля
 Author.objects.all().order_by('-rating_author').values('author__username', 'rating_author')[0]
 
 # Выводим самую лучшую статью
-best = Post.objects.all().order_by('-post_rating')[0]
-best_post = Post.objects.all().order_by('-post_rating').values(  # выводим параметры
+best = Post.objects.all().order_by('-rating_post')[0]
+best_post = Post.objects.all().order_by('-rating_post').values(  # выводим параметры
     'date_post',
-    'author__author__username',
+    'author_post__author__username',
     'header_post', 'rating_post',
     'text_post')[0]
 best.preview()
 
 # Выводим все комменты к статье
-Comment.objects.filter(post=best).values('author_comment__username', 'date_comment', 'text_comment')
+Comment.objects.filter(post_comment=best).values('author_comment__username', 'date_comment', 'text_comment')
