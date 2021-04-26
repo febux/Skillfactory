cb7965ca2306003ef643ed9029a11ee2bc72c63f
ALEXANDER <deadcove@mail.ru>
2021-04-26 18:24:21 +0300

    В консоль должны выводиться все сообщения уровня DEBUG и выше, включающие время, уровень сообщения, сообщения. Для сообщений WARNING и выше дополнительно должен выводиться путь к источнику события (используется аргумент pathname в форматировании). А для сообщений ERROR и CRITICAL еще должен выводить стэк ошибки (аргумент exc_info). Сюда должны попадать все сообщения с основного логгера django. В файл general.log должны выводиться сообщения уровня INFO и выше только с указанием времени, уровня логирования, модуля, в котором возникло сообщение (аргумент module) и само сообщение. Сюда также попадают сообщения с регистратора django В файл errors.log должны выводиться сообщения только уровня ERROR и CRITICAL. В сообщении указывается время, уровень логирования, само сообщение, путь к источнику сообщения и стэк ошибки. В этот файл должны попадать сообщения только из логгеров django.request, django.server, django.template, django.db_backends. В файл security.log должны попадать только сообщения, связанные с безопасностью, а значит только из логгера django.security. Формат вывода предполагает время, уровень логирования, модуль и сообщение. На почту должны отправляться сообщения уровней ERROR и выше из django.request и django.server, по формату как в errors.log, но без стэка ошибок. Более того, при помощи фильтров указать, что в консоль сообщения отправляются только при DEBUG = True, а на почту и в файл general.log только при DEBUG = False.

---
+++ ProjectD3_2/NewProject/simpleapp/models.py
@@ -0,0 +1,24 @@
+from django.db import models
+from django.core.validators import MinValueValidator
+
+
+# Создаём модель товара
+class Product(models.Model):
+    name = models.CharField(max_length=200)
+    price = models.FloatField(validators=[MinValueValidator(0.0, 'Price should be >= 0.0')])
+    quantity = models.IntegerField(validators=[MinValueValidator(0, 'Quantity should be >= 0')])
+    category = models.ForeignKey('Category', on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f'{self.name} {self.quantity}'
+
+    def get_absolute_url(self):  # добавим абсолютный путь чтобы после создания нас перебрасывало на страницу с товаром
+        return f'/products/{self.id}'
+
+
+#  создаём категорию, к которой будет привязываться товар
+class Category(models.Model):
+    name = models.CharField(max_length=200, unique=True)  # названия категорий тоже не должны повторяться
+
+    def __str__(self):
+        return f'{self.name.title()}'
---
+++ ProjectD3_2/NewProject/simpleapp/views.py
@@ -0,0 +1,63 @@
+from django.shortcuts import render
+from django.views.generic import ListView, UpdateView, CreateView, DetailView, DeleteView
+# импоритируем необходимые дженерики
+
+from django.core.paginator import Paginator
+
+from .models import Product, Category
+from .filters import ProductFilter
+from .forms import ProductForm
+
+
+# из списка на главной странице уберём всё лишнее
+class ProductsList(ListView):
+    model = Product
+    template_name = 'simpleapp/product_list.html'
+    context_object_name = 'products'
+    ordering = ['-price']
+    paginate_by = 1
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['filter'] = ProductFilter(self.request.GET, queryset=self.get_queryset())
+        return context
+
+
+# дженерик для получения деталей о товаре
+class ProductDetailView(DetailView):
+    template_name = 'simpleapp/product_detail.html'
+    queryset = Product.objects.all()
+
+
+# дженерик для создания объекта. Надо указать только имя шаблона и класс формы который мы написали в прошлом юните.
+# Остальное он сделает за вас
+class ProductCreateView(CreateView):
+    template_name = 'simpleapp/product_create.html'
+    form_class = ProductForm
+    success_url = '/products/'
+
+
+# дженерик для редактирования объекта
+class ProductUpdateView(UpdateView):
+    template_name = 'simpleapp/product_update.html'
+    form_class = ProductForm
+    success_url = '/products/'
+
+    # метод get_object мы используем вместо queryset,
+    # чтобы получить информацию об объекте который мы собираемся редактировать
+    def get_object(self, **kwargs):
+        id = self.kwargs.get('pk')
+        return Product.objects.get(pk=id)
+
+
+# дженерик для удаления товара
+class ProductDeleteView(DeleteView):
+    template_name = 'simpleapp/product_delete.html'
+    queryset = Product.objects.all()
+    success_url = '/products/'
+
+    # метод get_object мы используем вместо queryset,
+    # чтобы получить информацию об объекте который мы собираемся редактировать
+    def get_object(self, **kwargs):
+        id = self.kwargs.get('pk')
+        return Product.objects.get(pk=id)
